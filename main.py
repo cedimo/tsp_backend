e9f0312cb7a591da68b84f3ba6b458d90576f994
Johannes <johanneslehm@gmail.com>
2022-01-09 19:30:00 +0100

    api setup

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello TSP")
+from fastapi import Request, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# origins from which API calls are allowed
+origins = [
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/")
+async def test(request: Request):
+    x = await request.json()
+
+    # TODO
+
+    return x
