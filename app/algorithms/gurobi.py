57b2992bdfb8ec5d38be021f391446293f3224e8
Johannes Lehm <johanneslehm@gmail.com>
2022-05-03 10:16:01 +0200

    refactoring

--- app/algorithms/gurobi.py
+++ app/algorithms/gurobi.py
@@ -7,15 +7,12 @@ def subtourelim(model, where):
     if where == GRB.Callback.MIPSOL:
         # make a list of edges selected in the solution
         vals = model.cbGetSolution(model._vars)
-        selected = gp.tuplelist((i, j) for i, j in model._vars.keys()
-                                if vals[i, j] > 0.5)
+        selected = gp.tuplelist((i, j) for i, j in model._vars.keys() if vals[i, j] > 0.5)
         # find the shortest cycle in the selected edge list
         tour = subtour(selected)
         if len(tour) < n:
             # add subtour elimination constr. for every pair of cities in tour
-            model.cbLazy(gp.quicksum(model._vars[i, j]
-                                     for i, j in combinations(tour, 2))
-                         <= len(tour)-1)
+            model.cbLazy(gp.quicksum(model._vars[i, j] for i, j in combinations(tour, 2)) <= len(tour)-1)
 
 # Given a tuplelist of edges, find the shortest subtour
 def subtour(edges):
@@ -28,8 +25,7 @@ def subtour(edges):
             current = neighbors[0]
             thiscycle.append(current)
             unvisited.remove(current)
-            neighbors = [j for i, j in edges.select(current, '*')
-                         if j in unvisited]
+            neighbors = [j for i, j in edges.select(current, '*') if j in unvisited]
         if len(cycle) > len(thiscycle):
             cycle = thiscycle
     return cycle
@@ -42,15 +38,14 @@ def optimize(matrix):
     global n
     n = len(durations)
 
-    dist = {(i, j):
+    durations_dict = {(i, j):
         durations[i][j]
         for i in range(n) for j in range(i)}
-
 
     # GUROBI
     m = gp.Model()
 
-    vars = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY, name='e')
+    vars = m.addVars(durations_dict.keys(), obj=durations_dict, vtype=GRB.BINARY)
     for i, j in vars.keys():
         vars[j, i] = vars[i, j]
 
